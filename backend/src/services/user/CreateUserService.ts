cf007e071243b33256d61d8bd5eb38de94fc0522
Cristian Dreyer <cristianbr.dreyer@gmail.com>
2022-11-11 20:10:59 -0300

    adding users to database

--- backend/src/services/user/CreateUserService.ts
+++ backend/src/services/user/CreateUserService.ts
@@ -1,3 +1,6 @@
+import { prisma } from "@prisma/client";
+import prismaClient from "../../prisma"
+
 interface UserRequest {
     name: string;
     email: string;
@@ -7,11 +10,37 @@ interface UserRequest {
 class CreateUserService {
     async execute({ name, email, password }: UserRequest) {
 
-        return { 
-            name: name,
-            email: email,
-            password: password
+        //verificar se tem email
+        if (!email) {
+            throw new Error("Email incorrect")
         }
+
+        // verificar se ja tem email cadastrado
+        const userAlreadyExists = await prismaClient.user.findFirst({
+            where: {
+                email: email
+            }
+        })
+
+        if (userAlreadyExists) {
+            throw new Error("user already exists");
+        }
+
+        const user = await prismaClient.user.create({
+            data:{
+                name: name,
+                email: email,
+                password: password
+            },
+            select:{
+                id: true,
+                name: true,
+                email: true
+            }
+        })
+
+        return user;
     }
 }
+
 export { CreateUserService }
