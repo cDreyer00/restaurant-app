7b2c996a72806195306d5ab9330c8664124642a6
Cristian Dreyer <cristianbr.dreyer@gmail.com>
2022-11-11 14:31:14 -0300

    initial commit

---
+++ backend/src/server.ts
@@ -0,0 +1,27 @@
+import express, {Request, Response, NextFunction} from "express" 
+import "express-async-errors";
+import cors from "cors";
+
+import { router } from "./routes";
+
+const app = express();
+app.use(express.json()); // tipo de dado que vai ser usado
+app.use(cors()); // pra qualquer ip poder fazer requisição
+
+app.use(router); // usar as rotas criadas no routes
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    // se o err for do tipo "Error"
+    if (err instanceof Error){
+        return res.status(400).json({
+            error: err.message
+        })
+    }
+    
+    return res.status(500).json({
+        status: "error",
+        message: "internal server error"
+    })
+})
+
+app.listen(3333, () => console.log("Server running"))
